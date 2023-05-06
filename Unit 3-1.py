61073e4fa51a94dc410150b1fea1f1a09bd3c816
Vitchayut Suebwong <88417096+Vitchayut64070102@users.noreply.github.com>
2023-05-06 23:00:54 +0700

    Update Unit 3-1.py

--- Unit 3-1.py
+++ Unit 3-1.py
@@ -12,8 +12,6 @@ print(f'ผู้เล่นประเทศอังกฤษ: {eng_players}
 print(f'ผู้เล่นประเทศอิตาลี : {ita_players}')
 print('ตำแหน่งที่รับสมัคร: DF 1 คน, MF 1 คน, FW 1คน')
 
-
-
 # คำนวณค่าคาดหวังของจำนวนผู้ที่จะได้รับเลือก
 total_expected = 0
 for x in range(0, 4):
